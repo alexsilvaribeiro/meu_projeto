f955778f993d74e542e4e0c0eff5a446afee4d8b
AlexSilvaRibeiro <143361282+alexsilvaribeiro@users.noreply.github.com>
2024-02-23 08:51:08 -0300

    Update desenvolvimento_1_BDII.sql

--- desenvolvimento_1_BDII.sql
+++ desenvolvimento_1_BDII.sql
@@ -13,14 +13,14 @@ constraint foreign key (dono_id) references dono (id_dono)
 );
 
 insert into dono(nome_dono) values ('João'),
-								   ('Maria'),      
-		             	           ('José');
+				   ('Maria'),      
+		                   ('José');
 
                                       
 
 insert into animal(nome_animal, dono_id) values ('Caramelo','2'),
-												('Linguiça',null),
-												('beethoven','2'),
+						('Linguiça',null),
+						('beethoven','2'),
                                                 ('Brabo','1'),
                                                 ('Bigode',null),
                                                 ('Mel','2'),
